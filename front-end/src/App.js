41a573aa4e376a3f9a522461874fd79741e7d0f8
luanvalese <luan.valese@appmoove.com.br>
2022-06-17 17:21:03 -0300

    ajusting layout

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from "react";
 import logo from "./imgs/joker.svg";
 import jokehub from "./imgs/jokehub.png";
 import "./App.css";
-import { Input, Typography, Row, Button, Rate, List, Space } from "antd";
-import { AudioOutlined } from "@ant-design/icons";
+import { Input, Typography, Row, Button, Rate, List, Spin } from "antd";
+import { AudioOutlined, NodeExpandOutlined } from "@ant-design/icons";
 import {
   UserOutlined,
   LogoutOutlined,
@@ -17,6 +17,9 @@ const { Title, Link } = Typography;
 const { Search } = Input;
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
+
   const data = Array.from({
     length: 23,
   }).map((_, i) => ({
@@ -28,16 +31,19 @@ function App() {
 
   const onSearch = (value) => console.log(value);
 
-  const [user, setUser] = useState(null);
-
   const login = (username) => {
+    setLoading(true);
     localStorage.setItem("username", username);
     setUser(username);
+    setLoading(false);
   };
 
   const logout = () => {
+    setLoading(true);
     localStorage.setItem("username", "");
     setUser(null);
+    setLoading(false);
+    document.location.reload(true);
   };
 
   const inputUser = () => (
@@ -50,8 +56,14 @@ function App() {
       }
       placeholder="Enter your username"
       enterButton="Go"
+      allowClear
       size="large"
-      style={{ marginTop: 3, alignSelf: "center", maxWidth: "300px" }}
+      style={{
+        marginTop: 3,
+        alignSelf: "center",
+        maxWidth: "300px",
+        borderRadius: "16px",
+      }}
       onSearch={(username) => login(username)}
     />
   );
@@ -60,6 +72,7 @@ function App() {
     <Search
       enterButton="Go"
       size="large"
+      allowClear
       style={{ marginTop: 3, alignSelf: "center", width: "600px" }}
       onSearch={null}
     />
@@ -81,7 +94,7 @@ function App() {
               marginLeft: 10,
               marginTop: 10,
               alignSelf: "center",
-              color: "#00678c",
+              color: "rgb(3 110 172)",
             }}
           >
             Welcome {username}!
@@ -121,13 +134,15 @@ function App() {
           // style={{ display: "flex", justifyContent: "end"}}
           // key={item.title}
           actions={[
-            <Rate allowHalf disabled defaultValue={2.5} />,
             <Button>Save joke</Button>,
+            <Button type="link">Rate joke</Button>,
+            <Rate allowHalf disabled defaultValue={2.5} />,
           ]}
         >
           <List.Item.Meta title={<a href={item.href}>{item.title}</a>} />
           {item.content}
         </List.Item>
+        
       )}
     />
   );
@@ -138,49 +153,55 @@ function App() {
   }, [user]);
 
   return (
-    <body className="App-body">
-      <Row
-        class="App"
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          backgroundColor: "white",
-          width: 850,
-          height: "100%",
-          alignSelf: "center",
-        }}
+    <Spin tip="Loading..." spinning={loading}>
+      <body
+        className="App-body"
+        style={{ height: user || user === !"" ? "100%" : "100vh" }}
       >
-        <header style={{ display: "flex", justifyContent: "end" }}>
-          {userName(user)}
-        </header>
-        <img src={logo} className="App-logo" alt="logo" />
-        <img
-          src={jokehub}
-          alt="logo"
+        <Row
+          className="App"
           style={{
-            marginTop: 10,
+            display: "flex",
+            flexDirection: "column",
+            backgroundColor: user ? "white" : "none",
+            width: user ? 850 : "none",
+            minHeight: user ? 800 : "none",
             alignSelf: "center",
-            maxWidth: 170,
-            marginBottom: 20,
           }}
-        />
+        >
+          <header style={{ display: "flex", justifyContent: "end" }}>
+            {userName(user)}
+          </header>
+          <img src={logo} className="App-logo" alt="logo" />
+          <img
+            src={jokehub}
+            alt="logo"
+            className="Logo-text"
+            style={{
+              marginTop: 10,
+              alignSelf: "center",
+              width: 250,
+              marginBottom: 20,
+            }}
+          />
 
-        {user ? (
-          <>
-            <div style={{ alignSelf: "center" }}>
-              {inputJoke()}
-              <div style={{ display: "flex", justifyContent: "center" }}>
-                <Button type="link">My Jokes</Button>
-                <Button type="link">All jokes</Button>
+          {user ? (
+            <>
+              <div style={{ alignSelf: "center" }}>
+                {inputJoke()}
+                <div style={{ display: "flex", justifyContent: "center" }}>
+                  <Button type="link">My Jokes</Button>
+                  <Button type="link">All jokes</Button>
+                </div>
               </div>
-            </div>
-            {list()}
-          </>
-        ) : (
-          inputUser()
-        )}
-      </Row>
-    </body>
+              {list()}
+            </>
+          ) : (
+            inputUser()
+          )}
+        </Row>
+      </body>
+    </Spin>
   );
 }
 
